c777e3e590587d8635be32c0ce1b96ef85604619
Daniel Klimaj <klimaj@weps.cz>
2017-02-19 12:41:09 +0100

    [ADD] Basics

---
+++ proj1/ticket.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** *
+ * Projekt: POS #1 - Ticket algoritmus
+ * Autor:   Daniel Klimaj; xklima22@stud.fit.vutbr.cz
+ * Datum:   2017-02-19
+ * Subor:   ticket.c
+ * ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdib.h>
+
+int getticket();
+void await(int aenter);
+void advance();
+
+int main(int arg, char **argv)
+{
+  return 0;
+}
+
+int getticket()
+{
+  return 0;
+}
+
+void await(int aenter)
+{
+  printf("%d\n", aenter);
+}
+
+void advance()
+{
+}
