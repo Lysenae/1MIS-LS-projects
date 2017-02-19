831a584f9a4192750827e0c2f979a390f1440a54
Daniel Klimaj <klimaj@weps.cz>
2017-02-19 12:44:10 +0100

    [FIX] Compilable

--- proj1/ticket.c
+++ proj1/ticket.c
@@ -7,14 +7,19 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdib.h>
+#include <stdlib.h>
 
 int getticket();
 void await(int aenter);
 void advance();
 
-int main(int arg, char **argv)
+int main(int argc, char **argv)
 {
+  for(int i=0; i<argc; ++i)
+  {
+    printf("%s\n", argv[i]);
+  }
+
   return 0;
 }
 
