b38b55c107f95e0ae97600fce8634833e5f16041
Daniel Klimaj <klimaj@weps.cz>
2017-03-12 18:49:24 +0100

    [ADD][FLP1] Some fucking parsing stuff

--- FLP/fun/plg-2-nka.hs
+++ FLP/fun/plg-2-nka.hs
@@ -2,15 +2,17 @@ module Main
     (main)
   where
 
+import Control.Applicative
 import System.Environment
 import System.IO
 
 import Lib.CmdArgs.Parser
 import Lib.CmdArgs.Data
+import Lib.CmdArgs.Functions
 
 main :: IO ()
 main = do
-    c <- parseArguments <$> getArgs
-    input <- hGetContents stdin
-    print c
+  c <- parseArguments <$> getArgs
+  either print handleCmdArgs c
 
+handleCmdArgs (CmdArgs pi p1 p2 inrlg) = print (CmdArgs pi p1 p2 inrlg)
