4145e1a9d2b9fcc231de0089575677145b4aaf68
Daniel Klimaj <klimaj@weps.cz>
2017-03-14 17:19:20 +0100

    [MOD][FLP1] Command line args parsing

--- FLP/fun/Lib/CmdArgs/Parser.hs
+++ FLP/fun/Lib/CmdArgs/Parser.hs
@@ -5,16 +5,14 @@ module Lib.CmdArgs.Parser where
 import Lib.CmdArgs.Config
 import Lib.CmdArgs.Help
 
-parseArg :: [String] -> Either String Config -> Either String Config
-parseArg _ (Left msg) = (Left msg)
-parseArg [] (Right (Config pi p1 p2 inf)) = Right $ Config pi p1 p2 inf
-parseArg (a:as) (Right (Config pi p1 p2 inf))
-  | a == "-i"     = parseArg as (Right $ Config True p1 p2 inf)
-  | a == "-1"     = parseArg as (Right $ Config pi True p2 inf)
-  | a == "-2"     = parseArg as (Right $ Config pi p1 True inf)
-  | head a /= '-' && as == [] = Right $ Config pi p1 p2 a
+parseArg :: String -> String -> Either String Config
+parseArg a inf
+  | a == "-i"     = (Right $ Config True False False inf)
+  | a == "-1"     = (Right $ Config False True False inf)
+  | a == "-2"     = (Right $ Config False False True inf)
   | otherwise     = Left $ "Invalid option " ++ a
 
 parseArguments :: [String] -> Either String Config
-parseArguments []   = Left helpMsg
-parseArguments args = parseArg args (Right $ Config False False False "")
+parseArguments [x]   = parseArg x ""
+parseArguments [x,y] = parseArg x y
+parseArguments _     = Left helpMsg
