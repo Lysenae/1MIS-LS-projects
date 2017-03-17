2d77faaebe732ba7df7d4864f11b1ea291ce2cfc
Daniel Klimaj <klimaj@weps.cz>
2017-03-17 16:12:58 +0100

    [ADD][FLP1] Finished RLG transformation

--- FLP/fun/Lib/Rlg/Transform/SimpleRules.hs
+++ FLP/fun/Lib/Rlg/Transform/SimpleRules.hs
@@ -20,12 +20,7 @@ trSimpleRules (r:rs) all
   | otherwise   = [] ++ trSimpleRules rs all
 
 trSimpleRule :: Rule -> [Rule] -> [Rule]
-trSimpleRule r all = findDerivations (left r) (getSetNX (firstRSym r) all) all
-
--- findDerivations symX setN[SymX] allRules
-findDerivations :: Symbol -> [Symbol] -> [Rule] -> [Rule]
-findDerivations _ [] _         = []
-findDerivations x (s:ss) rules = findXRules x s rules ++ findDerivations x ss rules
+trSimpleRule r all = findXRules (left r) (firstRSym r) all
 
 -- find non-simple rules with specified symbol on the left side
 findXRules :: Symbol -> Symbol -> [Rule] -> [Rule]
@@ -33,10 +28,3 @@ findXRules _ _ [] = []
 findXRules x s (r:rs)
   | (left r) == s && chkRuleS r == False = [r {left = x}] ++ findXRules x s rs
   | otherwise = [] ++ findXRules x s rs
-
-getSetNX :: Symbol -> [Rule] -> [Symbol]
-getSetNX s [] = [s]
-getSetNX s (r:rs)
-  | chkRuleS r == False && (left r) == s && isNtermS (lastRSym r) =
-    lastRSym r `au` getSetNX s rs
-  | otherwise = [] `au` getSetNX s rs
