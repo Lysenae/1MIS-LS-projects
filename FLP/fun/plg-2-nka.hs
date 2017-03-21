a2502d41db6916b0d4d881510ced6e07628e66fd
Daniel Klimaj <klimaj@weps.cz>
2017-03-21 14:33:38 +0100

    [ADD][FLP1] Open file exception handling

--- FLP/fun/plg-2-nka.hs
+++ FLP/fun/plg-2-nka.hs
@@ -6,21 +6,21 @@ module Main
   where
 
 import Control.Applicative
+import Control.Exception
 import System.Environment
 import System.IO
+import System.IO.Error
 
 import Lib.Misc.Misc
-
 import Lib.CmdArgs.Parser
 import Lib.CmdArgs.Config
-
 import Lib.RLG.RLG
 import Lib.RLG.Handler
 
 main :: IO ()
 main = do
   c <- parseArguments <$> getArgs
-  either putStrLn handleCmdArgs c
+  either putStrLn handleCmdArgs c `catch` readExHandler
 
 handleCmdArgs :: Config -> IO ()
 handleCmdArgs conf = do
@@ -31,3 +31,6 @@ getInput :: FilePath -> IO String
 getInput inpath
   | emptyStr inpath = getContents
   | otherwise       = readFile inpath
+
+readExHandler :: IOError -> IO ()
+readExHandler e = putStrLn ("ERROR: " ++ show e)
