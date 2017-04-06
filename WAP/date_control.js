57ffbab52cda08a31fbc2a396d44280218ef8879
Daniel Klimaj <xklima22@stud.fit.vutbr.cz>
2017-04-06 16:03:15 +0200

    [ADD][WAP] Control table

--- WAP/date_control.js
+++ WAP/date_control.js
@@ -22,19 +22,101 @@ function onLoad()
 function buildControls(ctrl)
 {
   console.info("Building controls for '%s'", ctrl);
-  var timeElems = document.getElementsByTagName("time");
-  console.info("|<time>|: %d", timeElems.length);
-  for(i=0; i<timeElems.length; ++i)
-  {
-    appendControl(ctrl, i)
-  }
+  var times = document.getElementsByTagName("time");
+  console.info("|<time>|: %d", times.length);
+  buildControlsTable(ctrl, times)
 }
 
-function appendControl(ctrl, idx)
+function buildControlsTable(ctrl, times)
 {
-  var element = document.createElement("p");
-  var text    = document.createTextNode("Time " + idx);
-  element.appendChild(text);
-  ctrl.appendChild(element);
+  var table = document.createElement("table");
+  table.id  = "ctrlTable"
+  buildTableHeader(table);
+  for(i=0; i<times.length; ++i)
+  {
+    buildTableRows(table, times[i], i)
+  }
+  setControlTablePadding(table);
+  ctrl.appendChild(table);
+}
+
+function buildTableHeader(table)
+{
+  var th, text;
+  var tr   = document.createElement("tr");
+  th   = document.createElement("th");
+  text = document.createTextNode("Identification");
+  th.appendChild(text);
+  tr.appendChild(th);
+
+  th   = document.createElement("th");
+  text = document.createTextNode("Content");
+  th.appendChild(text);
+  tr.appendChild(th);
+
+  th   = document.createElement("th");
+  text = document.createTextNode("Show date?");
+  th.appendChild(text);
+  tr.appendChild(th);
+
+  th   = document.createElement("th");
+  text = document.createTextNode("Apply");
+  th.appendChild(text);
+  tr.appendChild(th);
+
+  table.appendChild(tr);
+}
+
+function buildTableRows(table, time, idx)
+{
+  var td, text, ip;
+  var tr = document.createElement("tr");
+  tr.className = idx;
+
+  td   = document.createElement("td");
+  text = document.createTextNode("Time " + idx);
+  td.appendChild(text);
+  tr.appendChild(td);
+
+  td   = document.createElement("td");
+  text = document.createElement("textarea");
+  text.setAttribute("rows", 1);
+  text.setAttribute("cols", 40);
+  text.style.resize  = "horizontal";
+  text.style.padding = "4px";
+  td.appendChild(text);
+  tr.appendChild(td);
+
+  td = document.createElement("td");
+  td.setAttribute("align", "center");
+  ip = document.createElement("input");
+  ip.setAttribute("type", "checkbox");
+  td.appendChild(ip);
+  tr.appendChild(td);
+
+  td = document.createElement("td");
+  td.setAttribute("align", "center");
+  ip = document.createElement("input");
+  ip.setAttribute("type", "submit");
+  ip.value = "Apply";
+  td.appendChild(ip);
+  tr.appendChild(td);
+
+  table.appendChild(tr);
+}
+
+function setControlTablePadding(table)
+{
+  var ths = table.getElementsByTagName("th");
+  var tds = table.getElementsByTagName("td");
+  for(i=0; i<ths.length; ++i)
+  {
+    ths[i].style.padding = "0 30px 0 30px";
+  }
+  for(i=0; i<tds.length; ++i)
+  {
+    console.info(i)
+    tds[i].style.padding = "0 45px 0 45px";
+  }
 }
 
