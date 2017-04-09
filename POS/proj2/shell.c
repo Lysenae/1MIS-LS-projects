b8cd0ff622add772c7fb606f5515361cc497babe
Daniel Klimaj <klimaj@weps.cz>
2017-04-09 17:25:49 +0200

    [ADD][POS2] Worker threads

--- POS/proj2/shell.c
+++ POS/proj2/shell.c
@@ -19,16 +19,34 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+#define CMDLEN 512
+
+typedef struct t_monitor
+{
+  bool            running;
+  char            command[CMDLEN+1];
+  pthread_mutex_t mtx_th;
+  pthread_mutex_t mtx_proc;
+  pthread_cond_t  cond;
+} TMonitor;
+
+void mt_init(TMonitor *m);
+void mt_shutdown(TMonitor *m);
+bool mt_running(TMonitor *m);
+
 void *th_read_rt(void *t);
 void *th_run_rt(void *t);
 
+TMonitor monitor;
+
 int main()
 {
+  mt_init(&monitor);
+
   pthread_t th_read, th_run;
 
-  int data = 1;
-  pthread_create(&th_read, NULL, th_read_rt, (void *) &data);
-  pthread_create(&th_run, NULL, th_run_rt, (void *) &data);
+  pthread_create(&th_read, NULL, th_read_rt, (void *) &monitor);
+  pthread_create(&th_run, NULL, th_run_rt, (void *) &monitor);
 
   pthread_join(th_read, NULL);
   pthread_join(th_run, NULL);
@@ -36,17 +54,40 @@ int main()
   return 0;
 }
 
+void mt_init(TMonitor *m)
+{
+  m->running = true;
+  memset(&m->command, 0, CMDLEN+1);
+}
+
+void mt_shutdown(TMonitor *m)
+{
+  pthread_mutex_lock(&m->mtx_th);
+  m->running = false;
+  pthread_mutex_unlock(&m->mtx_th);
+}
+
+bool mt_running(TMonitor *m)
+{
+  bool rslt;
+  pthread_mutex_lock(&m->mtx_th);
+  rslt = m->running;
+  pthread_mutex_unlock(&m->mtx_th);
+  return rslt;
+}
+
 void *th_read_rt(void *t)
 {
-  int d = *((int *) t);
-  printf("read %d\n", d);
+  TMonitor *m = (TMonitor *) t;
+  printf("read %s\n", mt_running(m) ? "true" : "false");
+  mt_shutdown(m);
   pthread_exit(NULL);
 
 }
 
 void *th_run_rt(void *t)
 {
-  int d = *((int *) t);
-  printf("run %d\n", d);
+  TMonitor *m = (TMonitor *) t;
+  printf("run %s\n", mt_running(m) ? "true" : "false");
   pthread_exit(NULL);
 }
