6af1b9cc7528d5c410e929be89ea93cff962bfae
Daniel Klimaj <klimaj@weps.cz>
2017-04-29 16:28:17 +0200

    [ADD][FLP2] Comments + fixes

--- FLP/log/flp17-log.pl
+++ FLP/log/flp17-log.pl
@@ -7,39 +7,44 @@
 main :-
   parse_stdin(Config),
   print_config(Config),
-  config_props(Config, S, L, R, N),
+  config(Config, S, L, R, N),
   write("Position: "),writeln(N),
   write("L: "),writeln(L),
   write("R: "),writeln(R),
   write("State: "),writeln(S),
   halt.
 
-% Spracovanie nacitanych udajov - zostavenie pasky a pravidiel TS
+% Spracovanie nacitanych udajov - zostavenie pasky a pravidiel TS.
+% Config - vystpuny parameter pre konfiguraciu TS
 parse_stdin(Config) :-
   prompt(_, ''),
   read_lines(Lines),
   maplist(remove_spaces, Lines, CleanLines),
-  get_config(Lines, C),
-  create_rules(CleanLines),
-  Config = C.
+  get_config(Lines, Config),
+  create_rules(CleanLines).
 
-% Odstrani medzery zo zoznamu
+% Odstrani medzery zo zoznamu.
+% L - zoznam
+% R - vysupny parameter pre vystupny zoznam
 remove_spaces([], []).
 remove_spaces([HL|TL], R) :-
   (HL == ' ', remove_spaces(TL, R));
   (HL \== ' ', remove_spaces(TL, Rs), R = [HL|Rs]).
 
-% Ziska pociatocnu konfiguraciu TS
+% Ziska pociatocnu konfiguraciu TS.
+% In     - zoznam
+% Config - vystupny parameter pre konfiguraciu
 get_config(In, Config) :-
   last(In, C),
   Config = ['S'|C]. % Nastavi pociatocny stav
 
-% Zostavi pravidla
+% Zostavi pravidla.
 create_rules(In) :-
   last(In, Last),
   delete(In, Last, In2), % Odstran pasku
   create_rules2(In2).
 
+% Zostavi pravidla zo stvorprvkoveho zoznamu.
 create_rules2([]).
 create_rules2([H|T]) :-
   nth0(0, H, SttP), % StatePresent  - sucasny stav
@@ -49,21 +54,26 @@ create_rules2([H|T]) :-
   assert(rule(SttP, SymP, SttN, SymN)),
   create_rules2(T).
 
-% Vypise aktualnu konfiguraciu
+% Vypise aktualnu konfiguraciu.
+% Config - konfiguracia TS
 print_config(Config) :-
   atomic_list_concat(Config, '', S),
   atom_string(S, R),
   writeln(R).
 
-% Najde aktualnu poziciu hlavy TS
+% Najde aktualnu poziciu hlavy TS.
+% Config - konfiguracia TS
+% Pos    - vystupny parameter pre poziciu hlavy na paske
 pos(Config, Pos) :-
   length(Config, Len),
   L is Len - 1,
   pos2(Config, L, P),
   Pos is P.
 
-% Vyhlada velke pismenu, ktore znaci poziciu hlavy, ak ho najde ulozi jeho
-% poziciu do OutPos.
+% Vyhlada velke pismeno, ktore znaci poziciu hlavy a vrati jeho poziciu v retazci.
+% Config - konfiguracia TS
+% CurPos - aktualne kontrolovana pozicia
+% OutPos - vystupny parameter pre poziciu hlavy
 pos2(Config, CurPos, OutPos) :-
   nth0(CurPos, Config, N),
   ( (char_type(N, upper), OutPos is CurPos);
@@ -72,11 +82,17 @@ pos2(Config, CurPos, OutPos) :-
     )
   ).
 
+% Ziska aktualny stav TS.
+% Config - konfiguracia TS
+% State  - vystupny parameter pre stav
 state(Config, State) :-
   pos(Config, Pos),
   nth0(Pos, Config, State).
 
-% Ziska retazec pred (L) a za (R) hlavou TS
+% Ziska retazec pred a za hlavou TS.
+% Config - prvy parameter, konfiguracia TS
+% L      - vystupny parameter pre retazec za (nalavo) hlavou
+% R      - vystupny parameter pre retazec pred (napravo) hlavou
 split_config([],[],[]).
 split_config([H|T],L,R) :- char_type(H, upper),R = T, L = [].
 split_config([H|T],L,R) :-
@@ -84,8 +100,13 @@ split_config([H|T],L,R) :-
   split_config(T,X,R),
   append([H],X,L).
 
-% Ziska vlastnosti konfiguracie - stav, retazec pred a za hlavou, poziciu
-config_props(Config, State, Left, Right, N) :-
+% Ziska vlastnosti konfiguracie - stav, retazec pred a za hlavou, poziciu.
+% Config - konfiguracia TS
+% State  - vystupny parameter pre stav TS
+% Left   - vystupny parameter pre retazec za (nalavo) hlavou
+% Right  - vystupny parameter pre retazec pred (napravo) hlavou
+% N      - vystupny parameter pre poziciu hlavy na paske
+config(Config, State, Left, Right, N) :-
   pos(Config, N),
   state(Config, State),
   split_config(Config, Left, Right).
